863fa058812fbe94ec93d1c3cc48137e9dc1ee0b
Bryon Bauer <bryon_bauer@outlook.com>
2019-09-28 14:33:23 -0500

    Added a Sub Menu

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,34 +8,54 @@ $(function () {
         $(".grid-container").css("margin-left", "20%");
     });
 
+    // Hide sidebar and moved grid-container back to original pos
     $("#menu-img-big").click(function () {
         $(".grid-container").css("margin-left", "3%");
         $("#sidebar").toggle();
     });
 
-    // Show dropdown menu when icon is hovered over and hide when cursor leaves
-    $(".dropdown").mouseenter(function () {
+    // Show dropdown when icon is hovered
+    $(".dropdown").hover(function () {
+        // over
+        $(".dropdown-content").show();
+        // Keep dropdown visible on hover
+        $(".dropdown-content").hover(function () {
+            // over
             $(".dropdown-content").show();
-        }),
-        $(".dropdown-content").mouseleave(function () {
+            // Show subMenus when clicked
+            $(".subDropdown").click(function (e) {
+                e.preventDefault();
+                $(".subDropdown-content").show();
+                // Keep dropdown visible when hovered and keep subMenu visible as well
+                $(".subDropdown-content").hover(function () {
+                    // over
+                    $(".dropdown-content").show();
+                    $(".subDropdown-content").show();
+                // If you leave subMenu then it hides and if you leave both they both hide
+                }, function () {
+                    // out
+                    $(".subDropdown-content").hide();
+                    $(".dropdown-content").hide();
+                });
+            });
+        }, function () {
+            // out
             $(".dropdown-content").hide();
-        }),
-        $(".dropdown-content").mouseenter(function () {
-            $(".dropdown-content").show();
-        }),
-        $(".dropdown").mouseleave(function () {
-            $(".dropdown-content").hide();
+            $(".subDropdown-content").hide();
         });
+    }, function () {
+        // out
+        $(".dropdown-content").hide();
+        $(".subDropdown-content").hide();
+    });
 
     // Show dashboard when icon is clicked
 
     $("#dashboard").click(function () {
         $(".dashboard-content").toggle();
-        $(".grid-container").css("margin-left", "25%");
     });
 
     $("#dashboard-big").click(function () {
         $(".dashboard-content").toggle();
-        $(".grid-container").css("margin-left", "3%");
     });
 });
